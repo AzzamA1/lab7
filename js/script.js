69f56b36dc557af053fb4e965dc69f46b022639f
AzzamHamdan <112507056+AzzamA1@users.noreply.github.com>
2022-11-08 03:35:55 -0600

    Added .value to input, added output

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 function init(){
   function alertMe(){
     var input = document.getElementById('entryinput');
-	alert('Azzam Abu Hamdan: ' + input);
+	document.getElementById('textoutput').innerHTML = input;
+	alert('Azzam Abu Hamdan: ' + input.value);
   }
 
 window.addEventListener('load', init);
